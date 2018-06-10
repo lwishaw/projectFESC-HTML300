af5a4c1ca77d1eb392abb94a6e9de65753bde5c9
lwishaw <32470775+lwishaw@users.noreply.github.com>
2018-06-10 03:14:33 -0700

    fix broken link

--- js/index.js
+++ js/index.js
@@ -1,61 +1,61 @@
-//
-//var logos = [
-//  {
-//    name: "University of Florida",
-//    imgSource: "images/logo1.jpg",
-//    url: "http://www.ufl.edu/"
-//  }, {
-//    name: "Florida State University",
-//    imgSource: "images/logo2.jpg",
-//    url: "http://www.fsu.edu/"
-//  }, {
-//    name: "University of Central Florida",
-//    imgSource: "images/logo3.jpg",
-//    url: "http://www.ucf.edu/"
-//  }, {
-//    name: "University of Southern Florida",
-//    imgSource: "images/logo4.jpg",
-//    url: "http://www.usf.edu/"
-//  }, {
-//    name: "Florida A&M University",
-//    imgSource: "images/logo5.jpg",
-//    url: "http://www.famu.edu/"
-//  }, {
-//    name: "Florida Atlantic University",
-//    imgSource: "images/logo6.jpg",
-//    url: "http://www.fau.edu/"
-//  }, {
-//    name: "Florida Gulf Coast University",
-//    imgSource: "images/logo7.jpg",
-//    url: "http://www.fgcu.edu/"
-//  }, {
-//    name: "Florida International University",
-//    imgSource: "images/logo8.jpg",
-//    url: "http://www.fiu.edu/"
-//  }, {
-//    name: "New College of Florida", 
-//    imgSource: "images/logo9.jpg",
-//    url: "http://www.ncf.edu/"
-//  }, {
-//    name: "University of North Florida",
-//    imgSource: "images/logo10.jpg",
-//    url: "http://www.unf.edu/"
-//  }, {
-//    name: "University of West Florida",
-//    imgSource: "images/logo11.jpg",
-//    url: "http://www.uwf.edu/"
-//  }, {
-//    name: "Florida Polytechnic",
-//    imgSource: "images/logo12.jpg",
-//    url: "https://floridapolytechnic.org/"
-//  }
-//]
-//new Vue({
-//  el: '#app',
-//  data: {
-//     universities: logos,
-//	}
-//})
+
+var logos = [
+  {
+    name: "University of Florida",
+    imgSource: "images/logo1.jpg",
+    url: "http://www.ufl.edu/"
+  }, {
+    name: "Florida State University",
+    imgSource: "images/logo2.jpg",
+    url: "http://www.fsu.edu/"
+  }, {
+    name: "University of Central Florida",
+    imgSource: "images/logo3.jpg",
+    url: "http://www.ucf.edu/"
+  }, {
+    name: "University of Southern Florida",
+    imgSource: "images/logo4.jpg",
+    url: "http://www.usf.edu/"
+  }, {
+    name: "Florida A&M University",
+    imgSource: "images/logo5.jpg",
+    url: "http://www.famu.edu/"
+  }, {
+    name: "Florida Atlantic University",
+    imgSource: "images/logo6.jpg",
+    url: "http://www.fau.edu/"
+  }, {
+    name: "Florida Gulf Coast University",
+    imgSource: "images/logo7.jpg",
+    url: "http://www.fgcu.edu/"
+  }, {
+    name: "Florida International University",
+    imgSource: "images/logo8.jpg",
+    url: "http://www.fiu.edu/"
+  }, {
+    name: "New College of Florida", 
+    imgSource: "images/logo9.jpg",
+    url: "http://www.ncf.edu/"
+  }, {
+    name: "University of North Florida",
+    imgSource: "images/logo10.jpg",
+    url: "http://www.unf.edu/"
+  }, {
+    name: "University of West Florida",
+    imgSource: "images/logo11.jpg",
+    url: "http://www.uwf.edu/"
+  }, {
+    name: "Florida Polytechnic",
+    imgSource: "images/logo12.jpg",
+    url: "https://floridapolytechnic.org/"
+  }
+]
+new Vue({
+  el: '#app',
+  data: {
+     universities: logos,
+	}
+})
 
 
 new Vue({
